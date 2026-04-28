9e25b62825c9caaa58af12c96c56635c3313b0c1
Ystyphn_Both <bothystyphn@gmail.com>
2026-04-28 15:57:14 +0800

    Added Faker

---
+++ Fakers/fake_state.gd
@@ -0,0 +1,24 @@
+## This is a fake class
+extends State
+class_name FakeState
+
+
+func enter(_transition: TransitionParameter):
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+# Just to mock something
+func initialize(e: Variant) -> void:
+	pass
+
+
+func physics_update(_delta: float) -> void:
+	pass
+
+
+func process_update(_delta: float) -> void:
+	pass
